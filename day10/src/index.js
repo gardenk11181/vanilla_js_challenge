29d6bfb7e5f38e42fe5183db061e9f1a53beb1a7
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-02-05 12:09:33 +0900

    Almost finish

--- day10/src/index.js
+++ day10/src/index.js
@@ -1,9 +1,26 @@
 const h2 = document.querySelector("h2");
 const form = document.querySelector("form");
 const slider = form.querySelector("input"); 
+const MESSAGE = "Generate a number between 0 and "
+const btnPlay = document.querySelector(".btnPlay");
+const result = document.querySelector(".result");
+const inputNumber = document.querySelector(".inputNumber");
+const winner = document.querySelector(".winner");
 
 function init(){
-    form.addEventListener()
+    slider.addEventListener('input',function(){
+        h2.innerText = MESSAGE+slider.value;
+    })
+    btnPlay.addEventListener('click',function(){
+        var random = Math.floor(Math.random()*slider.value+1);
+        result.innerText = "Your chose: "+inputNumber.value+", the machine chose: "+random;
+        if(slider.value===random) {
+            winner.innerText = "You won!"
+        } else{
+            winner.innerText = "You lost!"
+        }
+    })
+
 }
 
 init();
